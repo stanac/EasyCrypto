9010a8df0910858553e1a02ecc14b6b935f1046c
Milan Stanacev <milan@stanacev.com>
2016-12-21 20:02:11 +0100

    code coverage integration

--- runtests.ps1
+++ runtests.ps1
@@ -2,15 +2,9 @@ nuget install OpenCover -Version 4.6.519 -OutputDir nugettools
 
 nuget install coveralls.net -Version 0.7.0 -OutputDir nugettools
 
-cd EasyCrypto.Tests
+nuget install xunit.runner.console -Version 2.1.0 -OutputDir nugettools
 
-dotnet build
-
-..\nugettools\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test" -output:"..\coverageopencover.xml" -oldstyle -filter:"+[EasyCrypto]*"
-
-
-cd ..
-
+nugettools\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"nugettools\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe"  -targetargs:"EasyCrypto.Tests\bin\Debug\EasyCrypto.Tests.dll EasyCrypto.Tests\bin\Debug\EasyCrypto.dll -noshadow" -output:"coverageopencover.xml" -filter:"+[EasyCrypto*]* -[EasyCrypto.T*]* -[EasyCrypto*]EasyCrypto.Exc* -[xunit*]*"
 
 .\nugettools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i coverageopencover.xml --repoToken $env:COVERALL_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
 
